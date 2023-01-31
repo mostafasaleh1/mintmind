e98ed5070e06400f24b0bd4a13d76cfbedf9e4d9
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-01-31 14:44:24 +0200

    adding links to all buttons and adding hovering functions

---
+++ js/js.js
@@ -0,0 +1,10 @@
+function changeLogo(x, logo){
+    if(x == 1)
+    {
+        logo.src = "sourceImages/logo2.png"
+    }
+    if(x == 2)
+    {
+        logo.src = "sourceImages/logo.png"
+    }
+}
