91ae2fbc2b84d1afa2f2ba9d2da74fa1a6155d16
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-02-10 17:02:59 +0200

    replacing image sources with online sources

--- js/js.js
+++ js/js.js
@@ -1,10 +1,10 @@
 function changeLogo(x, logo){
     if(x == 1)
     {
-        logo.src = "sourceImages/logo2.png"
+        logo.src = "https://blogger.googleusercontent.com/img/a/AVvXsEg8qk_CRJhl9_tiO2eQabtHa80aBuzrOMKGbevQS1Ldj85bQF2pX2dZY-sCnwi990GfhP0rOnHgeLJLSMSx9iqGiL8Lp5VsXt3bFLVR3PTYkA7ilpemd4b3VqWV9-CY41wauezIYp0FaNXFcleo1QtVujrmoldoWV_tF8RsZE72eIa3PvyPiLCvapVN"
     }
     if(x == 2)
     {
-        logo.src = "sourceImages/logo.png"
+        logo.src = "https://blogger.googleusercontent.com/img/a/AVvXsEjVX9BH5eAEq9RDuTpjY06IkJRbC6OHz3gxMD6Q8Xgs8J6bLJXDm4zHXE2Tb2-e2pCCZWJzJYwiS4erCu_sAaBUyBriAlXI24woKND72SWd7d4ZvSmy6moPAN7p-OfDsRez92CWEtrMr-fW-4vOMB8M-4NjyxbXttWiNlOfLZDCj4A-zdzq8XojRsXB"
     }
 }
